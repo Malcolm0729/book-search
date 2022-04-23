dc883b15b91dc4b41eeffc4e4195ab01c376ebdf
Malcolm Hendrix <melanna_camacho@Melannas-MacBook-Pro.local>
2022-04-23 12:14:26 -0700

    making changes to app js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { ApolloProvider } from "@apollo/react-hooks";
 import ApolloClient from "apollo-boost";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import SearchBooks from "./pages/SearchBooks";
 import SavedBooks from "./pages/SavedBooks";
@@ -17,6 +17,7 @@ const client = new ApolloClient({
       },
     });
   },
+
   uri: "/graphql",
 });
 
